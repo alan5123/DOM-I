ca4476f13947de10a74d9b1df4113d89c17ccdb3
alan5123 <alanparker5123@gmail.com>
2020-02-10 16:44:05 -0500

    mvp

--- js/index.js
+++ js/index.js
@@ -38,5 +38,65 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent['cta']['img-src'])
+
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+
+
+let navigation = document.querySelector('nav');
+navigation.append('test')
+navigation.prepend('test2')
+let links = navigation.getElementsByTagName('a');
+
+links[0].textContent = 'Services';
+links[0].style.color = 'green';
+
+links[1].textContent = 'Product';
+links[1].style.color = 'green';
+
+links[2].textContent = 'Vision';
+links[2].style.color = 'green';
+
+links[3].textContent = 'Features';
+links[3].style.color = 'green';
+
+links[4].textContent = 'About';
+links[4].style.color = 'green';
+
+links[5].textContent = 'Contact';
+links[5].style.color = 'green';
+
+let topText = document.querySelector('h1');
+topText.innerHTML = 'Dom<br> is <br> awesome'
+
+let button = document.querySelector('button');
+button.innerHTML = 'Get Started';
+
+
+let pTitles = document.querySelectorAll ('h4');
+pTitles[0].textContent = 'Features'
+pTitles[1].textContent = 'About'
+pTitles[2].textContent = 'Services'
+pTitles[3].textContent = 'Product'
+pTitles[4].textContent = 'Vision'
+pTitles[5].textContent = 'Contact'
+
+
+let text = document.querySelectorAll ('p');
+
+text[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+text[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+text[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+text[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+text[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+text[5].innerHTML = '123 Way 456 Street </br> Somewhere, USA';
+text[6].textContent = '1 (888) 888-8888';
+text[7].textContent = 'sales@greatidea.io';
+text[8].textContent = 'Copyright Great Idea! 2018';
